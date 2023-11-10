e3b1beee7efa33fa118c72debfb7bb06b8103d10
Léo Jansen <leo@leonardojansen.com>
2023-11-10 09:14:36 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,6 +4,14 @@ const Home = () => {
   return (
     <div>
       <section className='w-full flex-center flex-col'>
+        <h1 className='head_text text-center'>
+          Discover & Share
+          <br className='max-md:hidden'/>
+          <span></span>
+        </h1>
+        <p className='desc text-center'>
+          Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts
+        </p>
 
       </section>
     </div>
